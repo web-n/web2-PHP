8f5d5b37b54b9e1b002b9ecbb4a55295b231063e
egoing <egoing@gmail.com>
2018-01-24 02:12:23 +0900

    조건문 예제

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title></title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
       <li><a href="index.php?id=HTML">HTML</a></li>
       <li><a href="index.php?id=CSS">CSS</a></li>
@@ -13,11 +13,19 @@
     </ol>
     <h2>
       <?php
+      if(isset($_GET['id'])){
         echo $_GET['id'];
+      } else {
+        echo "Welcome";
+      }
       ?>
     </h2>
     <?php
-    echo file_get_contents("data/".$_GET['id']);
+    if(isset($_GET['id'])){
+      echo file_get_contents("data/".$_GET['id']);
+    } else {
+      echo "Hello, PHP";
+    }
      ?>
   </body>
 </html>
