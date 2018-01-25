c2406ed7f9041cefc4e142bec81e5dec5ca78ecf
egoing <egoing@gmail.com>
2018-01-25 22:06:44 +0900

    글쓰기

--- create.php
+++ create.php
@@ -44,13 +44,16 @@ function print_list(){
       ?>
     </ol>
     <a href="create.php">create</a>
-    <h2>
-      <?php
-      print_title();
-      ?>
-    </h2>
-    <?php
-    print_description();
-     ?>
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+        <textarea name="description" placeholder="Description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
   </body>
 </html>
