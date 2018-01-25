aa5922fa6c4675acebaba92da3b39a5d2bb2fd2d
egoing <egoing@gmail.com>
2018-01-25 17:07:54 +0900

    함수의 형식 1

---
+++ function2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>function</title>
+  </head>
+  <body>
+    <h1>Function</h1>
+    <h2>Basic</h2>
+    <?php
+      function basic(){
+        print("Lorem ipsum dolor1<br>");
+        print("Lorem ipsum dolor2<br>");
+      }
+      basic();
+      basic();
+      basic();
+    ?>
+    
+  </body>
+</html>
