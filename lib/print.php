3d12053540c79e2f59f0da0ce36dea1c1ddb92b5
egoing <egoing@gmail.com>
2018-01-26 02:05:43 +0900

    cross site scripting 보안 적용

--- lib/print.php
+++ lib/print.php
@@ -1,14 +1,14 @@
 <?php
 function print_title(){
   if(isset($_GET['id'])){
-    echo $_GET['id'];
+    echo htmlspecialchars($_GET['id']);
   } else {
     echo "Welcome";
   }
 }
 function print_description(){
   if(isset($_GET['id'])){
-    echo file_get_contents("data/".$_GET['id']);
+    echo htmlspecialchars(file_get_contents("data/".$_GET['id']));
   } else {
     echo "Hello, PHP";
   }
@@ -17,9 +17,10 @@ function print_list(){
   $list = scandir('./data');
   $i = 0;
   while($i < count($list)){
+    $title = htmlspecialchars($list[$i]);
     if($list[$i] != '.') {
       if($list[$i] != '..') {
-        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+        echo "<li><a href=\"index.php?id=$title\">$title</a></li>\n";
       }
     }
     $i = $i + 1;
