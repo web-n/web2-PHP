35614748877a306759ec17649494b29a9d70fac4
egoing <egoing@gmail.com>
2018-01-25 17:17:28 +0900

    함수의 형식 3

--- function2.php
+++ function2.php
@@ -25,5 +25,16 @@
     sum(2,4);
     sum(4,6);
     ?>
+    <h2>return</h2>
+    <?php
+    function sum2($left, $right){
+      return $left+$right;
+    }
+    print(sum2(2,4));
+    file_put_contents('result.txt', sum2(2,4));
+    // email('egoing@egoing.net', sum2(2,4));
+    // upload('egoing.net', sum2(2,4));
+
+    ?>
   </body>
 </html>
